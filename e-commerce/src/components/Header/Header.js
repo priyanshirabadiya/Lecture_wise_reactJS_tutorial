5b29fa8f3f21c2ca9bb034c77a71a2ecf52d56f0
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-04-18 22:04:18 +0530

    18/4/24

---
+++ e-commerce/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from  '../assets/logo.png'
+
+export default function Header() {
+  return (
+    <>
+    <img src={logo} alt="" />
+    </>      
+  )
+}
+
+
