96cea82bba7ce5debe7fb50ba6ee097a6eddfdfc
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-04-21 22:53:06 +0530

    21/4/24

--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import logo from  '../assets/logo.png'
+import logo from '../assets/logo.png'
+import './header.css'
 
 export default function Header() {
   return (
     <>
-    <img src={logo} alt="" />
-    </>      
+      <div className='container grid-cols-2 gap-5 pt-7 grid-rows-1 mx-auto flex justify-between px-7'>
+        <div className='logo-div col '><img src={logo} alt="" /></div>
+        <div className='flex' >
+          <div>
+            <form action="#">
+              <div class="search-info p-relative">
+                <button class="header-search-icon">
+                  <i class="fal fa-search"></i>
+                </button>
+                <input placeholder="Search products..." type="search" />
+              </div>
+            </form>
+          </div>
+          <div>sdf</div>
+        </div>
+      </div>
+    </>
   )
 }
 
