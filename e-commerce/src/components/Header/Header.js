1305b213f90f83b88bd000e3dce0e4c9225ebcbe
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-04-22 14:54:15 +0530

    22/4/24

--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -20,8 +20,8 @@ export default function Header() {
           </div>
           <div className='three-cart' >
             <div className="f-selector">
-              <div class="dropdown">
-                <button class="dropbtn">Dropdown</button>
+              <div class="dropdown  ">
+                <button class="dropbtn rounded-md">Dropdown</button>
                 <div class="dropdown-content">
                   <a href="#">Link 1</a>
                   <a href="#">Link 2</a>
