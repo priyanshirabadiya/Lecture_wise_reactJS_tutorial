98e1d83a751ee3ccf1716c5a77454c0c10f0e0c9
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-04-23 11:51:33 +0530

    23/4/24

--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -1,37 +1,59 @@
 import React from 'react'
 import logo from '../assets/logo.png'
+import flag from '../assets/flag.png'
 import './header.css'
+import './responsive.css'
 
 export default function Header() {
   return (
     <>
       <div className='container grid-cols-2 gap-5 pt-7 grid-rows-1 mx-auto flex justify-between px-7'>
-        <div className='logo-div col '><img src={logo} alt="" /></div>
-        <div className='flex' >
-          <div>
-            <form action="#">
-              <div class="search-info p-relative rounded">
-                <button class="header-search-icon">
-                  <i class="fa-solid fa-magnifying-glass" style={{ color: "#5d5f5f", paddingRight: "7px" }}></i>
-                </button>
-                <input placeholder="Search products..." type="text" />
-              </div>
-            </form>
+
+        <div className='logo-div flex justify-between'>
+          <div className='side-icon'>
+            <i className="fa-solid fa-bars  absolute fa-lg top-14 "></i>
           </div>
-          <div className='three-cart' >
-            <div className="f-selector">
-              <div class="dropdown  ">
-                <button class="dropbtn rounded-md">Dropdown</button>
-                <div class="dropdown-content">
-                  <a href="#">Link 1</a>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
-                </div>
-              </div>
-
+          <div>
+            <img src={logo} alt="" className='absolute ms-6 top-9' />
+          </div>
+        </div>
+        <div className='flex' >
+          <form action="#">
+            <div className="search-info p-relative rounded">
+              <button className="header-search-icon">
+                <i className="fa-solid fa-magnifying-glass" style={{ color: "#5d5f5f", paddingRight: "7px" }}></i>
+              </button>
+              <input placeholder="Search products..." type="text" />
             </div>
+          </form>
 
-
+          <div className='three-cart flex' >
+            <div className="dropdown ms-9 border rounded">
+              <button className="dropbtn rounded-md flex"> <img src={flag} alt="" /><b>English</b><i className="fa-solid fa-angle-down fa-sm" style={{ color: "#858585" }}></i></button>
+              <div className="dropdown-content">
+                <a href="#">Arabin</a>
+                <a href="#">Spanish</a>
+                <a href="#">Mandrain</a>
+              </div>
+            </div>
+            <div className="s-selector">
+              <select className='border h-12 rounded ms-3' >
+                <option>USD</option>
+                <option>YEAN</option>
+                <option>EURO</option>
+              </select>
+            </div>
+            <div className='mt-3 ms-3'>
+              <span className="material-symbols-outlined relative">shopping_cart</span>
+              <span className="tp-product-count">2</span>
+            </div>
+            <div className='ms-3 mt-3 fa-lg' >
+              <i className="fa-regular fa-user"></i>
+            </div>
+            <div className='mt-3 ms-3'>
+              <i className="fa-regular fa-heart fa-lg"></i>
+              <span className="tp-product-count">1</span>
+            </div>
           </div>
         </div>
       </div>
