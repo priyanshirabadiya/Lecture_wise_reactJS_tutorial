2582e4428cf3e57c4374bd32b6d264bbdcf6d4b1
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-05-04 11:16:47 +0530

    4/5/24

--- e-commerce/src/components/N_Pages/Sign_In/Sign_in.js
+++ e-commerce/src/components/N_Pages/Sign_In/Sign_in.js
@@ -30,6 +30,39 @@ export default function Sign_in() {
                 </div>
             </div>
             <div className="flex xl:flex-nowrap justify-center xl:justify-between mb-10 flex-wrap mx-10 mt-20">
+
+                <div className="h-[750px] bg-[#F6F6F6] rounded md:mt-10 lg:mt-0 ">
+                    <div className=' rounded-t-lg relative' >
+                        <img src={signinI2} className='rounded-t-lg' alt="" />
+                    </div>
+                    <div className='pt-10 ps-10 flex' >
+                        <div>
+                            <button className='p-4 rounded bg-white'><TbShoppingBagMinus className='text-[#d12531]' /></button>
+                        </div>
+                        <div>
+                            <div className='ms-2'>
+                                <h3 className='font-bold  '>Sign Up</h3>
+                                <p className='text-[0.9rem] leading-6 mt-2 text-gray-500 w-[35vw]' >Your personal data will be used to support your experience throughout this website, to manage access to your account.</p>
+                            </div>
+                            <div className=' bg-white py-1 mt-5 rounded flex -ms-12 ' >
+                                <CiMail className='m-4 me-0 ' />
+                                <input type="text" className='px-2 pe-72 py-3 focus:border-none ' placeholder='Username/email address' />
+                            </div>
+                            <div className=' bg-white py-1 mt-3 rounded flex -ms-12' >
+                                <GoKey className='m-4 me-0 text-gray-500 ' />
+                                <input type="text" className='px-2 pe-72 py-3 focus:border-none ' placeholder='Password' />
+                            </div>
+                            <div className="flex mt-5 justify-between -ms-12 n-check">
+                                <a href="" className='underline text-[0.75rem] mt-2' >Already Have Account?</a>
+                            </div>
+                            <div className=' bg-[#F3EEE7] py-4  rounded mt-5 flex -ms-12 hover:bg-[black] hover:text-white
+                                hover:transition-all hover:duration-500 hover:ease-in-out justify-center'>
+                                <button>Register here </button><IoIosArrowRoundForward className='mt-1 ms-2 h-7 w-6' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="h-[750px] bg-[#F6F6F6] rounded ">
                     <div className=' rounded-t-lg relative' >
                         <img src={signinI} className='rounded-t-lg' alt="" />
@@ -67,37 +100,6 @@ export default function Sign_in() {
                     </div>
                 </div>
 
-                <div className="h-[750px] bg-[#F6F6F6] rounded md:mt-10 lg:mt-0 ">
-                    <div className=' rounded-t-lg relative' >
-                        <img src={signinI2} className='rounded-t-lg' alt="" />
-                    </div>
-                    <div className='pt-10 ps-10 flex' >
-                        <div>
-                            <button className='p-4 rounded bg-white'><TbShoppingBagMinus className='text-[#d12531]' /></button>
-                        </div>
-                        <div>
-                            <div className='ms-2'>
-                                <h3 className='font-bold  '>Sign Up</h3>
-                                <p className='text-[0.9rem] leading-6 mt-2 text-gray-500 w-[35vw]' >Your personal data will be used to support your experience throughout this website, to manage access to your account.</p>
-                            </div>
-                            <div className=' bg-white py-1 mt-5 rounded flex -ms-12 ' >
-                                <CiMail className='m-4 me-0 ' />
-                                <input type="text" className='px-2 pe-72 py-3 focus:border-none ' placeholder='Username/email address' />
-                            </div>
-                            <div className=' bg-white py-1 mt-3 rounded flex -ms-12' >
-                                <GoKey className='m-4 me-0 text-gray-500 ' />
-                                <input type="text" className='px-2 pe-72 py-3 focus:border-none ' placeholder='Password' />
-                            </div>
-                            <div className="flex mt-5 justify-between -ms-12 n-check">
-                                <a href="" className='underline text-[0.75rem] mt-2' >Already Have Account?</a>
-                            </div>
-                            <div className=' bg-[#F3EEE7] py-4  rounded mt-5 flex -ms-12 hover:bg-[black] hover:text-white
-                                hover:transition-all hover:duration-500 hover:ease-in-out justify-center'>
-                                <button>Login Now </button><IoIosArrowRoundForward className='mt-1 ms-2 h-7 w-6' />
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
 
         </>
