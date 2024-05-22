9bfee8c71c45d74e4e8074675a6940d98504747f
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-05-22 19:56:12 +0530

    22/5/24

--- crudapi/src/App.js
+++ crudapi/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
@@ -6,16 +5,17 @@ import Edit from './components/Edit';
 import Login from './components/Login';
 import Register from './components/Register';
 import Create from './components/Create';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Home />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/create" element={<Edit />}></Route>
-          <Route path="/edit" element={<Login />}></Route>
-          <Route path="/login" element={<Register />}></Route>
-          <Route path="/register" element={<Create />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/edit" element={<Edit />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
