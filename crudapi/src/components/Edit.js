eefcfa13cb556aa1f22548bb0fca99c8532dbe9d
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-05-24 08:55:22 +0530

    24/5/24

--- crudapi/src/components/Edit.js
+++ crudapi/src/components/Edit.js
@@ -1,61 +1,101 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Home from './Home';
-import Navbar from './Navbar';
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+import React, { useEffect, useState } from 'react'
+import Navbar from './Navbar'
+import axios from "axios";
 
 export default function Edit() {
+  const [data, setdata] = useState([]);
+
+  const loaduser = async () => {
+    const res = await axios.get("http://localhost:3001/users");
+    setdata(res.data);
+    // setdata(res);
+    // console.log(res , "res"); //in this you will find data in this field
+    console.log(res.data, "res"); // so we find data from this perticular filed
+  }
+  useEffect(() => {
+    loaduser();
+  }, [])
+
   return (
-    <>
-    <Navbar/>
     <div>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <div>
+        <Navbar />
+      </div>
+      <section class="mx-auto w-full max-w-7xl px-4 py-4">
+        <div class="flex flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0">
+          <div>
+            <h2 class="text-lg font-semibold">Employees</h2>
+            <p class="mt-1 text-sm text-gray-700">
+              This is a list of all employees. You can add new employees, edit or
+              delete existing ones.
+            </p>
+          </div>
+          <div>
+            <button
+              type="button"
+              class="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+            >
+              Add new employee
+            </button>
+          </div>
+        </div>
+        <div class="mt-6 flex flex-col">
+          <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div class="overflow-hidden border border-gray-200 md:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                  <thead class="bg-gray-50">
+                    <tr>
+                      <th scope="col" class="relative py-3.5 text-left ps-10">Employee</th>
+                      <th scope="col" class="relative py-3.5">Title</th>
+                      <th scope="col" class="relative py-3.5" > Status</th>
+                      <th scope="col" class="relative py-3.5" > Role</th>
+                      <th scope="col" class="relative py-3.5 text-end pe-10">Edit</th>
+                    </tr>
+                  </thead>
+                  <tbody class="divide-y divide-gray-200 bg-white">
+                    {data.map((cvalue, index, arr) => (
+                      <tr className=''>
+                        <td class=" whitespace-nowrap py-4 ps-8 ">
+                          <div class="flex items-center">
+                            <div class="h-10 w-10 flex-shrink-0"  >
+                              <img src={cvalue.img} style={{ borderRadius: "100%", height: "40px", width: "40px", backgroundPosition: "center", objectFit: "cover" }} />
+                            </div>
+                            <div class="ml-4">
+                              <div class="text-sm font-medium text-gray-900">
+                                {cvalue.name}
+                              </div>
+                              <div class="text-sm text-gray-700">{cvalue.email}</div>
+                            </div>
+                          </div>
+                        </td>
+                        <td class="whitespace-nowrap px-12 py-4">
+                          <div class="text-sm text-gray-900 ">{cvalue.title}</div>
+                          <div class="text-sm text-gray-700">Engineering</div>
+                        </td>
+                        <td class="whitespace-nowrap px-4 py-4">
+                          <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                            Remove
+                          </span>
+                        </td>
+                        <td class="whitespace-nowrap px-4 py-4 text-sm text-gray-700">
+                          {cvalue.role}
+                        </td>
+                        <td class="whitespace-nowrap px-10 py-4 text-right text-sm font-medium">
+                          <a href="#" class="text-gray-700">
+                            Edit
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
     </div>
-    </>
   )
 }
--- crudapi/src/components/Navbar.js
+++ crudapi/src/components/Navbar.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
     return (    
         <>
-            <nav class="bg-white border-gray-200 dark:bg-gray-400 dark:border-gray-400">
+            <nav class="bg-black border-gray-200 dark:bg-black dark:border-gray-400">
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                     <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
                         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Holders</span>
                     </a>
                     <button data-collapse-toggle="navbar-multi-level" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
@@ -16,7 +16,7 @@ export default function Navbar() {
                         </svg>
                     </button>
                     <div class="hidden w-full md:block md:w-auto" id="navbar-multi-level">
-                        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-400 md:dark:bg-gray-400 dark:border-gray-700">
+                        <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border bg-black md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0   dark:border-gray-700">
                             <li>
                                 <Link to="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:p-0 md:hover:text-blue-700 md:dark:bg-transparent" aria-current="page">Home</Link>
                             </li>
@@ -43,4 +43,138 @@ export default function Navbar() {
         </>
     )
 }
+
+
+
+// import React from 'react'
+// import { Link } from 'react-router-dom';
+// import bg from './backg.png';
+
+// export default function Navbar() {
+//     return (
+//         <div>
+//             <div class="relative w-full bg-gray-600 p-3">
+//                 <div class="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+//                     <div class="inline-flex items-center space-x-2">
+//                         <span className='' >
+//                             <img src={bg} height={50} width={30} alt="" />
+//                         </span>
+//                         <span class="font-bold">DevUI</span>
+//                     </div>
+//                     <div class="hidden grow items-start lg:flex">
+//                         <ul class="ml-12 inline-flex space-x-8">
+//                             <li>
+//                                 <Link
+//                                     to="/"
+//                                     class="inline-flex items-center text-sm font-semibold text-white hover:text-gray-900"
+//                                 >
+//                                 Home
+//                                     <span>
+//                                         <svg
+//                                             xmlns="http://www.w3.org/2000/svg"
+//                                             width="24"
+//                                             height="24"
+//                                             viewBox="0 0 24 24"
+//                                             fill="none"
+//                                             stroke="currentColor"
+//                                             stroke-width="2"
+//                                             stroke-linecap="round"
+//                                             stroke-linejoin="round"
+//                                             class="ml-2 h-4 w-4"
+//                                         >
+//                                             <polyline points="6 9 12 15 18 9"></polyline>
+//                                         </svg>
+//                                     </span>
+//                                 </Link>
+//                             </li>
+//                             <li>
+//                                 <a
+//                                     href="#"
+//                                     class="inline-flex items-center text-sm font-semibold text-white hover:text-gray-900"
+//                                 >
+//                                     About
+//                                     <span>
+//                                         <svg
+//                                             xmlns="http://www.w3.org/2000/svg"
+//                                             width="24"
+//                                             height="24"
+//                                             viewBox="0 0 24 24"
+//                                             fill="none"
+//                                             stroke="currentColor"
+//                                             stroke-width="2"
+//                                             stroke-linecap="round"
+//                                             stroke-linejoin="round"
+//                                             class="ml-2 h-4 w-4"
+//                                         >
+//                                             <polyline points="6 9 12 15 18 9"></polyline>
+//                                         </svg>
+//                                     </span>
+//                                 </a>
+//                             </li>
+//                             <li>
+//                                 <a
+//                                     href="#"
+//                                     class="inline-flex items-center text-sm font-semibold text-white hover:text-gray-900"
+//                                 >
+//                                     Contact
+//                                     <span>
+//                                         <svg
+//                                             xmlns="http://www.w3.org/2000/svg"
+//                                             width="24"
+//                                             height="24"
+//                                             viewBox="0 0 24 24"
+//                                             fill="none"
+//                                             stroke="currentColor"
+//                                             stroke-width="2"
+//                                             stroke-linecap="round"
+//                                             stroke-linejoin="round"
+//                                             class="ml-2 h-4 w-4"
+//                                         >
+//                                             <polyline points="6 9 12 15 18 9"></polyline>
+//                                         </svg>
+//                                     </span>
+//                                 </a>
+//                             </li>
+//                         </ul>
+//                     </div>
+//                     <div class="hidden space-x-2 lg:block">
+//                         <Link to="/signin">
+//                         <button
+//                             type="button"
+//                             class="rounded-md bg-transparent px-3 py-2 text-sm font-semibold text-black hover:bg-black/10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+//                         >
+//                             Sign In
+//                         </button>
+//                         </Link>
+//                         <button
+//                             type="button"
+//                             class="rounded-md border border-black px-3 py-2 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+//                         >
+//                             Log In
+//                         </button>
+//                     </div>
+//                     <div class="lg:hidden">
+//                         <svg
+//                             xmlns="http://www.w3.org/2000/svg"
+//                             width="24"
+//                             height="24"
+//                             viewBox="0 0 24 24"
+//                             fill="none"
+//                             stroke="currentColor"
+//                             stroke-width="2"
+//                             stroke-linecap="round"
+//                             stroke-linejoin="round"
+//                             class="h-6 w-6 cursor-pointer"
+//                         >
+//                             <line x1="4" y1="12" x2="20" y2="12"></line>
+//                             <line x1="4" y1="6" x2="20" y2="6"></line>
+//                             <line x1="4" y1="18" x2="20" y2="18"></line>
+//                         </svg>
+//                     </div>
+//                 </div>
+//             </div>
+
+//         </div>
+//     )
+// }
 
