ee94700d7652209d2030693a48f0d0ea52524a37
priyanshirabadiya <rabadiyapriyansi@gmail.com>
2024-06-24 20:12:42 +0530

    24/6/24

---
+++ my-project/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import Time from  './tasks/A_Time';
+import Props from './tasks/B_cards'
+import img from './assets/boy.webp';
+import img2 from './assets/man.jpg';
+import img3 from './assets/women.avif';
+import TernaryOp from './tasks/C_Emojs';
+import Textform from './tasks/D_incrementdec';
+
+function App() {
+  return (
+    <>
+    {/* good mrning / evening / nignt project */}
+      {/* <Time></Time> */}
+
+    {/* 3 cards project */}
+      {/* <h1 className='text-center mt-4 text-4xl font-bold' >New three components</h1>
+      <div className='flex w-screen items-center justify-center flex-wrap '>
+      <Props name="David Dell" para="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates repudiandae. " img ={img}  ></Props>
+      <Props name="Rose Bush" para="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates repudiandae." img = {img2} ></Props>
+      <Props name="Jones Gli" para="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates repudiandae." img = {img3} ></Props>
+      </div> */}
+
+      {/* Emojs logo compare equal or not  */}
+
+      {/* <TernaryOp/> */}
+
+      {/* increment derement project */}
+      <Textform/>
+
+      </>
+);
+}
+
+export default App;
+
+
+
+
+/*
+ */
+
+
+
+
+
+
